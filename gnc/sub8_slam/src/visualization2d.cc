65c4aa42ae29bddd9350038fa12490d0e85c6574
Jacob Panikulam <jpanikulam@ufl.edu>
2016-01-10 00:46:56 -0500

    SLAM: Add nicer reprojection visualization

--- gnc/sub8_slam/src/visualization2d.cc
+++ gnc/sub8_slam/src/visualization2d.cc
@@ -6,13 +6,12 @@ void draw_points(cv::Mat& frame, const PointVector& points, int radius, int thic
   // TODO: Let user pass a color
   for (unsigned int k = 0; k < points.size(); k++) {
     cv::circle(frame, points[k], radius, cv::Scalar(240, 0, 0), thickness);
-    // todo: red rectangle outlines would look cooler
+    // TODO: red rectangle outlines would look cooler
   }
 }
 
 void draw_point_ids(cv::Mat& frame, const PointVector& points, const IdVector& point_ids) {
   for (unsigned int k = 0; k < points.size(); k++) {
-    // cv::circle(frame, points[k], radius, cv::Scalar(240), 1);
     cv::putText(frame, std::to_string(point_ids[k]), points[k], cv::FONT_HERSHEY_SIMPLEX, 0.5,
                 cv::Scalar(0, 250, 0));
   }
@@ -25,6 +24,10 @@ void draw_reprojection(cv::Mat& frame, const Point3Vector& points3d, const Pose&
   cv::Mat rotation_vector;
   cv::Rodrigues(pose.rotation, rotation_vector);
   cv::projectPoints(points3d, rotation_vector, pose.translation, K, cv::Mat(), points2d_est);
-  draw_points(frame, points2d_est, 2, -1);
+
+  PointVector points_est = points2d_est; // casting tho
+  for (unsigned int k = 0; k < points_est.size(); k++) {
+    cv::circle(frame, points_est[k], 2, cv::Scalar(10, 10, 240), -1);
+  }
 }
 }
